b50631f960b8bc9279a644b27ca9a006cf513b1a
Gaby-Gar <125601824+Gaby-Gar@users.noreply.github.com>
2023-05-02 21:01:40 -0600

    Add files via upload

---
+++ BucleWhille/src/BucleWhille.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author LENOVO
+ */
+public class BucleWhille {
+    public void Infinito(){
+        int n = 0;
+        while (n<1){
+           System.out.println("1 si es menor a 2");
+        }   
+    }
+    
+    public static void main(String[] args) {
+        BucleWhille Bucle = new BucleWhille();
+        Bucle.Infinito();
+    }
+    
+}
---
+++ CiclosInfinitos1/src/BucleFor.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author LENOVO
+ */
+public class BucleFor {
+
+    public void Infinito(){
+        for (int i = 0; i < 1; i++){
+            i = i - 1;
+            System.out.println("el valor de i= "+ i);
+        } 
+    }
+    
+    public static void main(String[] args) {
+        BucleFor Bucle = new BucleFor();
+        Bucle.Infinito();
+    }
+    
+}
